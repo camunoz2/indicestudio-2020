51d54e95c4dea3e490c44416a82baae0963b8cf1
indicestudio <indicestudio@gmail.com>
2020-12-03 22:54:08 -0300

    header animations

--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,54 @@
 import React from "react"
 import style from "./button.module.css"
+import {motion} from "framer-motion"
+
+const buttonTopFramer = {
+    before: {
+        y: -77,
+        x: -10,
+        opacity: 0,
+        scale: 0.8
+    },
+    after: {
+        y: -62,
+        x: 0,
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delay: 1.3,
+        }
+    }
+}
+
+const buttonBoderFramer = {
+    before: {
+        y: -10,
+        x: -20,
+        opacity: 0,
+        scale: 0.8
+    },
+    after: {
+        y: 0,
+        x: -10,
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delay: 1.4,
+            damping: 1
+        }
+    }
+}
 
 const Button = ({content}) => {
+
+
     return (
         <div className={style.button}>
-            <div className={style.buttonTop} >
+            <motion.div variants={buttonBoderFramer} initial="before" animate="after" className={style.buttonBorder} >
+            </motion.div>
+            <motion.div whileHover={{backgroundColor:"#FFF878"}}  variants={buttonTopFramer} initial="before" animate="after" className={style.buttonTop} >
                 {content}
-            </div>
-            <div className={style.buttonBorder} >
-            </div>
+            </motion.div>
         </div>
     )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 import variables from "../theme/variables.module.css"
 import styles from "../components/header.module.css"
@@ -9,28 +9,124 @@ import imgLanding02 from "../images/landing02.png"
 import Button from "../components/button"
 import IndiceLogo from './icons/indice_logo'
 
+const logoFramer = {
+  before: {
+    opacity: 0,
+  },
+  after: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5
+    }
+  }
+}
+
+const logoItemFramer = {
+  before: {
+    opacity: 0,
+    y: -10
+  },
+  after: {
+    opacity: 1,
+    y: 0
+  }
+}
+
+const letterVariants = {
+    before: {
+      opacity: 0,
+      y: 100,
+      transition: {
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+      },
+    },
+    after: {
+      opacity: 1,
+      y: 0,
+      transition: {
+          delay: 0.7,
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+      },
+    },
+  }
+
+  const imageUp = {
+    before: {
+      opacity: 0,
+      y: 300,
+      transition: {
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+      },
+    },
+    after: {
+      opacity: 1,
+      y: 180,
+      transition: {
+          delay: 0.9,
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+      },
+    },
+  }
+
+  const imageDown = {
+    before: {
+      opacity: 0,
+      y: -500,
+      transition: {
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+      },
+    },
+    after: {
+      opacity: 1,
+      y: -100,
+      transition: {
+          delay: 1.1,
+          type: "spring",
+          damping: 16,
+          stiffness: 200,
+      },
+    },
+  }
+
 const Header = () => {
+
+
     return(
         <div className={styles.header}>
-            <div className={`${styles.indiceLogo} ${styles.relative}`}>
-                <motion.div animate={{scale:[0, 1, 2, 1]}} transition={{duration:0.4}} className={styles.indiceIcon}>
+            <motion.div variants={logoFramer} initial="before" animate="after" className={`${styles.indiceLogo} ${styles.relative}`}>
+                <motion.div variants={logoItemFramer} className={styles.indiceIcon}>
                     <IndiceLogo />
                 </motion.div>
-                <motion.h1 animate={{scale:[0, 1]}} transition={{duration:0.4}} className={`${variables.fontDisplay} ${variables.indiceBlue}`}>Indicestudio</motion.h1>
-            </div>
+                <motion.h1 variants={logoItemFramer} className={`${variables.fontDisplay} ${variables.indiceBlue}`}>Indicestudio</motion.h1>
+            </motion.div>
             {/* <div className={styles.line}>
                 <SvgLine/>
             </div> */}
-            <img className={styles.landingImgBottom} src={imgLanding02} alt="mano sosteniendo un velular hacia arriba sacando una foto" />
-            <img className={styles.landingImgTop} src={imgLanding01} alt="indicestudio celular en mano al reves" />
+            <motion.img variants={imageUp} initial="before" animate="after" className={styles.landingImgBottom} src={imgLanding02} alt="mano sosteniendo un velular hacia arriba sacando una foto" />
+            <motion.img variants={imageDown} initial="before" animate="after" className={styles.landingImgTop} src={imgLanding01} alt="indicestudio celular en mano al reves" />
 
-            <p className={styles.headerText}>
-            Comienza a ver los beneficios inmediatos de una buena presencia online.
-            </p>
+            <motion.p
+                className={styles.headerText}
+                variants={letterVariants}
+                initial="before"
+                animate="after"
+                >
+                Comienza a ver los beneficios inmediatos de una buena presencia online.
+            </motion.p>
             
             <div className={styles.homeCta}>
-                <Button content="contacto"/>
-                <Button content="portafolio"/>
+                  <Button content="contacto"/>
+                  <Button content="portafolio"/>
             </div>
 
         </div>
